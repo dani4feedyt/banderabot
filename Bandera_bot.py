c4d7773b74a4515807fc0d2e4b8a78f9a597f8e1
Dan <dan.arbatov@ukr.net>
2021-03-11 20:39:19 +0200

    good

--- Bandera_bot.py
+++ Bandera_bot.py
@@ -110,9 +110,9 @@ try:
         await ctx.send(embed=embed)
     
     @bot.command(pass_context=True)
-    async def clear(ctx, amount = 8192):
+    async def clear(ctx, amount = 100):
         await ctx.channel.purge(limit=int(amount))
-        if amount == 8192:
+        if amount >= 100:
             amount = 'дуууууже багато'
         time.sleep(0.75)    
         await ctx.send(f'Будо видалено {amount} повідомлень!', delete_after=60)
