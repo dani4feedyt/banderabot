0eb18d318c203625877b99e22543b64160a09bb0
Dan <dan.arbatov@ukr.net>
2021-03-25 13:24:12 +0200

    good

--- Bandera_bot.py
+++ Bandera_bot.py
@@ -104,8 +104,8 @@ try:
         except asyncio.TimeoutError:
             print("Error")
         else:
-            await channel.send(f"{user} залишив сервер")
-        await user.kick(reason = reason)
+            await ctx.send(f"{user} залишив сервер")
+            await user.kick(reason = reason)
 
     @bot.command()
     async def pasta(ctx, pa: int):
