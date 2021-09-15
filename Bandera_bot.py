2486b378f6d7dabd79d9fae3970b89c02e0619a9
Dan <dan.arbatov@ukr.net>
2021-09-15 20:40:07 +0300

    good

--- Bandera_bot.py
+++ Bandera_bot.py
@@ -37,9 +37,8 @@ try:
     w = ("Bandera_bot.py")
     
     @bot.event
-    async def on_member_join(ctx, member):
-        guild = ctx.guild
-        await member.send(f"Вітаємо вас на сервері {ctx.guild.name}!\nЯ - **Бандера бот**, ваш персональний помічник, створений *dani4feedyt#5200*, який допоможе вам швидко зрозуміти правила та порядки серверу.\nДля отримання більш розгорнутої інформації, перейдіть до каналу **#info**")
+    async def on_member_join(member):\w
+        await member.send(f"Вітаємо вас на сервері!\nЯ - **Бандера бот**, ваш персональний помічник, створений *dani4feedyt#5200*, який допоможе вам швидко зрозуміти правила та порядки серверу.\nДля отримання більш розгорнутої інформації, перейдіть до каналу **#info**")
         await member.send("https://media.discordapp.net/attachments/618165831943061791/819546666272161802/CSuO7F_wPr0.png?width=541&height=676")
         
     @bot.event
