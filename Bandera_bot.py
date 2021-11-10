f217a95793e14980f0eb5972222b8e8504606885
Dan <dan.arbatov@ukr.net>
2021-11-10 15:50:24 +0200

    good

--- Bandera_bot.py
+++ Bandera_bot.py
@@ -212,7 +212,7 @@ try:
         embed.add_field(name=f"**b!rg8421**", value=f"???", inline=inline)
         embed.set_image(url="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/%D0%A2%D1%80%D0%B0%D0%B4%D0%B8%D1%86%D1%96%D1%8F_%D1%96_%D0%9F%D0%BE%D1%80%D1%8F%D0%B4%D0%BE%D0%BA.jpg/200px-%D0%A2%D1%80%D0%B0%D0%B4%D0%B8%D1%86%D1%96%D1%8F_%D1%96_%D0%9F%D0%BE%D1%80%D1%8F%D0%B4%D0%BE%D0%BA.jpg")
         embed.add_field(name=f"**Запрошення на найбазованіший сервер**", value=f"https://discord.gg/Ty5FcmEQkj", inline=inline)
-        embed.add_field(name=f"||Команди з поміткою {zaha_emoji} може використовувати тільки модерація||\n\n\n*Розробник:* **@dani4feedyt#5200**", value="*ver 2.2.1*", inline=inline)
+        embed.add_field(name=f"||Команди з поміткою {zaha_emoji} може використовувати тільки модерація||\n\n\n*Розробник:*" + " " + '<@' + str(486176412953346049) + '>', value='*ver 2.2.12*', inline=inline)
         
         await ctx.send(embed=embed)
         
@@ -256,7 +256,7 @@ try:
         except asyncio.TimeoutError:
             print("Error")
         else:
-            await ctx.send(f"{user} залишив сервер")
+            await ctx.send(f'{user} був виключений з серверу модератором **{author.mention}**, {reason}{reasonA}')
             await user.send(f'Ви були виключені з серверу **{guild.name}** модератором **{author.mention}**, {reason}{reasonA}')
             await user.kick(reason = reason)
      
@@ -438,7 +438,7 @@ try:
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Помилка. Будь ласка, введіть усі необхідні параметри.\n||**b!kick @(Нікнейм) (Причина)**||")
         if isinstance(error, commands.MemberNotFound):
-            await ctx.send("Помилка. Користувача з таким нікнеймом не будо знайдено. Можливо, нікнейм будо введено некоректно")
+            await ctx.send("Помилка. Користувача з таким нікнеймом не будо знайдено. Можливо, нікнейм будо введено некоректно, або цього користувача немає на сервері")
 
     @pasta.error
     async def pasta_error(ctx, error):
