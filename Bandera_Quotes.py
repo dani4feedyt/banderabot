044369221421f92ee09cd5dd11a43e950094b553
Dan <dan.arbatov@ukr.net>
2021-11-27 22:35:49 +0200

    good

--- Bandera_Quotes.py
+++ Bandera_Quotes.py
@@ -11,7 +11,8 @@ import pandas as pd
 a1 = 1
 b1 = 1
 c1 = 1
-n = 22
-page = requests.get('https://porokhivnytsya.com.ua/2018/12/30/stepan-bandera_quotes/')
-soup = BeautifulSoup(page.content, 'html.parser')
-_dict0 = soup.find_all('p', text=True)[5:n]
+#n = 22
+#page = requests.get('https://porokhivnytsya.com.ua/2018/12/30/stepan-bandera_quotes/')
+#soup = BeautifulSoup(page.content, 'html.parser')
+#_dict0 = soup.find_all('p', text=True)[5:n]
+_dict0 = 0
