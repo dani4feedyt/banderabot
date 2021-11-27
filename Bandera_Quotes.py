3405ea9ac0931dfd47a6f019f8620174bbf15938
Dan <dan.arbatov@ukr.net>
2021-11-27 20:05:02 +0200

    good

--- Bandera_Quotes.py
+++ Bandera_Quotes.py
@@ -8,17 +8,19 @@ from collections import Counter
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-a1 = 1
-b1 = 1
-c1 = 1
-page = requests.get('https://porokhivnytsya.com.ua/2018/12/30/stepan-bandera_quotes/')
-soup = BeautifulSoup(page.content, 'html.parser')
-_dict1 = soup.find_all('p')[5:37]
-i = 0
-_dict0 = []
-while i <= 31:
-    for dct in _dict1:
-        dct1 = _dict1[i].get_text()
-        i += 1
-        _dict0.append(dct1)
-
+try:
+    a1 = 1
+    b1 = 1
+    c1 = 1
+    page = requests.get('https://porokhivnytsya.com.ua/2018/12/30/stepan-bandera_quotes/')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    _dict1 = soup.find_all('p')[5:37]
+    i = 0
+    _dict0 = []
+    while i <= 31:
+        for dct in _dict1:
+            dct1 = _dict1[i].get_text()
+            _dict0.append(dct1)
+            i += 1
+except IndexError as err:
+    pass
