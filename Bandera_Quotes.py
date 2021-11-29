8e7d3394b79d1bb598878fac0ec069bd6ad61163
Dan <dan.arbatov@ukr.net>
2021-11-29 19:15:09 +0200

    good

--- Bandera_Quotes.py
+++ Bandera_Quotes.py
@@ -14,9 +14,11 @@ b1 = 1
 c1 = 1
 n_1 = 22
 pag = Request('https://porokhivnytsya.com.ua/2018/12/30/stepan-bandera_quotes/',  headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(pag).read()
+webpage = urlopen(pag, delay = 1).read()
 page = webpage.decode('utf-8')
+print(page)
 soup = BeautifulSoup(page, 'html.parser')
+
 _dict0 = soup.find_all('p', text=True)[5:n_1]
 print(_dict0)
 if _dict0 != []:
