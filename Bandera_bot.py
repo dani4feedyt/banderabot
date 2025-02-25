235eb2ea01dbbf55e80f91f1857aad10bb12e66e
dani4feed <dan.arbatov@ukr.net>
2025-02-25 22:14:29 +0200

    25.02.25 merge to heroku

--- Bandera_bot.py
+++ Bandera_bot.py
@@ -189,7 +189,7 @@ try:
         await interaction.edit_original_response(content=f"Я гадаю, що це... {output_labels}")
 
 
-    # TODO Указать в таблице правил количество наказания в минутахx
+    # TODO Указать в таблице правил количество наказания в минутах
 
     @bot.event
     async def on_message(message):
